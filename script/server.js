1dde3253593130f48c00b830f629fbf10d9b77c1
Kiwi <ivajimalo@alu.edu.gva.es>
2024-12-18 16:44:51 +0100

    Pues al final nada xd

--- script/server.js
+++ script/server.js
@@ -5,27 +5,6 @@ import { turso } from './database.js';
 const app = express();
 const port = 3000;
 
-// -----------------------------------------------------------------------
-// -----------------------------------------------------------------------
-// -----------------------------------------------------------------------
-
-const path = require('path');
-const PORT2 = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname)));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(port, () => {
-    console.log(`Servidor escuchando en el puerto ${port}`);
-});
-
-// -----------------------------------------------------------------------
-// -----------------------------------------------------------------------
-// -----------------------------------------------------------------------
-
 app.use(cors());
 app.use(express.json());
 
